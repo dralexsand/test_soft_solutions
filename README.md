bbae0ea2fcb8b09d14e84a1e878ca42e194129a9
dralexsand <dr.alex.sand.111@gmail.com>
2020-11-04 12:35:40 +0600

    correct

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
 <a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
@@ -7,55 +6,85 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
 </p>
 
-## About Laravel
+## Laravel, Vue, Vuex, Bootstrap
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+Порядок действий
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+composer global require laravel/installer
 
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
+composer create-project --prefer-dist laravel/laravel blog
 
-## Learning Laravel
+composer require laravel/ui
 
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
+php artisan ui vue
 
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+npm install && npm run dev
 
-## Laravel Sponsors
 
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
+php artisan make:model Car -m
+php artisan make:model Brand -m
+php artisan make:model CarModel -m
+php artisan make:model Color -m
+php artisan make:model Transmission -m
 
-### Premium Partners
+php artisan migrate
 
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Cubet Techno Labs](https://cubettech.com)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[Many](https://www.many.co.uk)**
-- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
-- **[DevSquad](https://devsquad.com)**
-- **[OP.GG](https://op.gg)**
+RouteServiceProvider, 
+	add prefix to api, /v1
+	separation of routes by basic access level (admin, user, web)
 
-## Contributing
 
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+php artisan make:request CarRequest
+php artisan make:request BrandRequest
+php artisan make:request CarModelRequest
+php artisan make:request ColorRequest
+php artisan make:request TransmissionRequest
 
-## Code of Conduct
+php artisan make:controller Api/v1/CarController --api
 
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
+php artisan make:seeder CarSeeder
+php artisan make:seeder BrandSeeder
+php artisan make:seeder CarModelSeeder
+php artisan make:seeder ColorSeeder
+php artisan make:seeder TransmissionSeeder
 
-## Security Vulnerabilities
 
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
+mkdir Traits in app directory
+create trait SeederDataTrait in new file SeederDataTrait.php
 
-## License
+Add 'use SeederDataTrait' in 
+TransmissionSeeder, CarModelSeeder, BrandSeeder, CarSeeder 
 
-The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+composer dump-autoload
+
+
+php artisan db:seed --class=TransmissionSeeder
+
+php artisan db:seed --class=BrandSeeder
+
+php artisan db:seed --class=ColorSeeder
+
+php artisan db:seed --class=CarModelSeeder
+
+php artisan db:seed --class=CarSeeder
+
+
+npm install vue bootstrap-vue bootstrap
+npm i vue-router
+
+mkdir resources/js/router
+create resources/js/router/index.js as router
+npm install vuex --save
+
+mkdir resources/js/store
+create resources/js/store/index.js as store
+
+
+create resources/js/store/modules/cars.js
+create resources/js/store/modules/filters.js
+create resources/js/store/modules/orders.js
+
+
+
+На данный момент работа выполнена ориентировочно на 70%
+
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,17 +1,389 @@
 <template>
-    <div>
-        <h1>Home.vue</h1>
+    <div class="container-fluid">
 
-        <list_cars/>
+        <h1>Cars</h1>
+
+        <div v-if="errored" class="alert alert-danger" role="alert">
+            No records
+        </div>
+
+        <div v-else class="row">
+
+            <div class="col-md-3">
+
+                <table class="table">
+                    <thead>
+                    <th>Filter</th>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Brands</td>
+                    </tr>
+                    <tr>
+                        <td>
+
+                            <BoxItem
+                                box_name="BMW, Germany"
+                                box_id="1"
+                                box_class="b"
+                            >
+                            </BoxItem>
+
+                            <BoxItem
+                                box_name="Audi, Germany"
+                                box_id="7"
+                                box_class="b"
+                            >
+                            </BoxItem>
+
+                            <BoxItem
+                                box_name="Wolkswagen, Germany"
+                                box_id="11"
+                                box_class="b"
+                            >
+                            </BoxItem>
+
+                        </td>
+                    </tr>
+
+                    <Divider/>
+
+                    <tr>
+                        <td>
+
+                            <BoxItem
+                                box_name="BMW, Germany"
+                                box_id="3"
+                                box_class="m"
+                            >
+                            </BoxItem>
+
+                            <BoxItem
+                                box_name="Audi, Germany"
+                                box_id="5"
+                                box_class="m"
+                            >
+                            </BoxItem>
+
+                            <BoxItem
+                                box_name="Wolkswagen, Germany"
+                                box_id="7"
+                                box_class="m"
+                            >
+                            </BoxItem>
+
+                        </td>
+                    </tr>
+
+                    <Divider/>
+
+                    <tr>
+                        <td>
+
+                            <BoxPrice
+                                min_price="50"
+                                max_price="10000000"
+                            >
+                            </BoxPrice>
+
+                        </td>
+                    </tr>
+
+                    <Divider/>
+
+                    <tr>
+                        <td>
+
+                            <BoxItem
+                                box_name="Manual"
+                                box_id="1"
+                                box_class="t"
+                            >
+                            </BoxItem>
+
+                            <BoxItem
+                                box_name="Automatic"
+                                box_id="2"
+                                box_class="t"
+                            >
+                            </BoxItem>
+
+                        </td>
+                    </tr>
+
+                    <Divider/>
+
+                    <tr>
+                        <td>
+
+                            <BoxItem
+                                v-for="color in filter_list_item.colors"
+                                box_name="color.name"
+                                box_id="color.id"
+                                box_class="c"
+                            >
+                            </BoxItem>
+
+                        </td>
+                    </tr>
+
+
+                    </tbody>
+                </table>
+
+            </div>
+            <div class="col-md-9">
+
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li
+                            :class="{disabled: !pagination.prev_page_url}"
+                            @click.prevent="getPosts(pagination.prev_page_url)"
+                            class="page-item">
+                            <a
+                                aria-label="Previous"
+                                class="page-link"
+                                href=""
+                            >
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+
+                        <li
+                            class="page-item disabled">
+                            <a class="page-link" href="">
+                                Page is {{ pagination.current_page }} of {{ pagination.last_page }}
+                            </a>
+                        </li>
+
+                        <li
+                            :class="{disabled: !pagination.next_page_url}"
+                            @click.prevent="getPosts(pagination.next_page_url)"
+                            class="page-item">
+                            <a
+                                aria-label="Next"
+                                class="page-link"
+                                href=""
+                            >
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </nav>
+
+                <table class="table table-striped">
+
+                    <div v-if="loading">Loading...</div>
+
+                    <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Photo</th>
+                        <th scope="col">car_created_year</th>
+                        <th scope="col">car_number</th>
+                        <th scope="col">price_per_day</th>
+
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="post in posts" :key="post.id">
+
+                        <td>{{ post.id }}</td>
+                        <td>{{ post.photo }}</td>
+                        <td>{{ post.car_created_year }}</td>
+                        <td>{{ post.car_number }}</td>
+                        <td>{{ post.price_per_day }}</td>
+                        <td>
+                            <button class="btn btn-success">
+                                Edit
+                            </button>
+                            <button class="btn btn-danger">
+                                Delete
+                            </button>
+                        </td>
+
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <button
+            @click="applyFilter()"
+            type="button"
+            class="btn btn-primary btn_apply">
+            Aplly
+        </button>
 
     </div>
 </template>
+
 <script>
-import ItemCars from "../components/ItemCars";
+
+import Divider from "../components/Divider";
+import BoxItem from "../components/BoxItem";
+import BoxPrice from "../components/BoxPrice";
 
 export default {
+    props: {
+        box: {
+            box_class: 'default_class',
+            box_id: 'default_id',
+            box_description: 'Default description',
+        },
+        checkboxes_list: {
+            checkbox_id: 'germany',
+            checkbox_class: 'germany_bmw',
+            checkbox_description: 'BMW - auto from Germany',
+        }
+    },
     components: {
-        'list_cars': ItemCars
+        BoxItem, Divider, BoxPrice
+    },
+    data: function () {
+        return {
+            posts: [],
+            post: {
+                id: '',
+                photo: '',
+                car_created_year: '',
+                car_number: '',
+                price_per_day: '',
+            },
+            post_id: '',
+            pagination: {},
+            edit: false,
+            loading: true,
+            errored: false,
+
+            filter_data: {},
+
+            fdata: {
+                brands: [],
+                models: [],
+            },
+
+            filter_list: [],
+            filter_list_item: {
+                colors: []
+            },
+            /*colorItem: {
+                id: '',
+                name: ''
+            }*/
+
+
+        }
+    },
+    mounted() {
+        this.getFilterPanel();
+        this.getPosts();
+    },
+    methods: {
+        getFilterPanel() {
+            axios
+                .get('/api/v1/list_filters')
+                .then(response => {
+                    let filter_list = response.data;
+                    this.filter_list = filter_list;
+                    console.log('FILTER_LIST');
+                    console.log(filter_list);
+
+                    this.filter_list_item.colors = filter_list.colors;
+                    console.log('FILTER_LIST_COLORS');
+                    console.log(filter_list.colors);
+
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.errored = true;
+                })
+                .finally(() => this.loading = false);
+
+        },
+
+
+        applyFilter() {
+            let boxes = this.$store.getters['filters/GET_FILTERS'];
+            console.log('STATE FILTERS');
+            console.log(boxes);
+            this.postFilter();
+        },
+
+        postFilter() {
+
+            let postBody = {
+                'models': [
+                    'Camry', 'Crown'
+                ],
+                'brands': [
+                    'Porshe'
+                ],
+            }
+
+            axios.post(`/api/v1/filters`, {
+                body: postBody
+                //body: this.postBody
+            })
+                .then(response => {
+                    console.log('POST RESPONSE');
+                    console.log(response);
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
+        },
+
+        paginateTable(response) {
+            let pagination = {
+                current_page: response.current_page,
+                last_page: response.last_page,
+                prev_page_url: response.prev_page_url,
+                next_page_url: response.next_page_url,
+
+                per_page: response.per_page,
+                total: response.total
+            }
+            this.pagination = pagination;
+            //console.log(this.pagination);
+        }
+        ,
+        getPosts(page_url) {
+
+            page_url = page_url || '/api/v1/cars';
+
+            axios
+                .get(page_url)
+                .then(response => {
+                    let posts = response.data.data;
+                    this.posts = posts;
+                    this.paginateTable(response.data)
+                    //console.log(posts);
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.errored = true;
+                })
+                .finally(() => this.loading = false);
+        }
+        ,
     }
 }
 </script>
+<style>
+.btn_apply {
+    position: fixed;
+    top: 100px;
+    left: 10px;
+}
+
+.btn_get_state {
+    position: fixed;
+    top: 200px;
+    left: 10px;
+}
+
+</style>
